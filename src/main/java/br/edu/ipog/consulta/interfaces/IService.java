bd754e930d60490b3af78611f2fcad638d2f89ed
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-07 20:43:07 -0300

    chore: setup project

--- src/main/java/br/edu/ipog/consulta/interfaces/IService.java
+++ src/main/java/br/edu/ipog/consulta/interfaces/IService.java
@@ -1,5 +1,7 @@
 package br.edu.ipog.consulta.interfaces;
 
+import java.util.List;
+
 /**
  * @see <a href="https://docs.oracle.com/javase/tutorial/java/generics/types.html">...</a>
  * T = Type (aluno,pessoa,carro, etc)
@@ -26,7 +28,31 @@ public interface IService<T,N> {
      */
     public T readById(N id);
 
+    /**
+     * Retorna uma lista de List<T>
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/List.html">...</a>
+     * @return
+     */
+    public List<T> read();
 
+    /**
+     *
+     * @param id para identificar o registro a ser atualizado
+     * @param entity valores que serão atualizado durante a operação de persitência
+     * @return
+     */
+    public T update(N id, T entity);
 
+    /**
+     *
+     * @param id - identificador do registro a ser deletado;
+     */
+    public T deleteById(N id);
 
+    /**
+     *
+     * @param entity - entidade que será removida do sistema;
+     * @return
+     */
+    public T delete(T entity);
 }
