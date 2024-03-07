4975f5282f8f65def0c17098d4739f948f07dc55
Bruno Urbano <brunourb@hotmail.com>
2024-03-07 19:51:21 -0300

    first commit

---
+++ src/main/java/br/edu/ipog/consulta/model/Aluno.java
@@ -0,0 +1,17 @@
+package br.edu.ipog.consulta.model;
+
+import jakarta.persistence.*;
+
+@Entity //ORM gerencia o estado objeto a ser criado à partir da classe concreta
+@Table(name = "ALUNO") //cria uma tabela chamada aluno
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "NOME", length = 50, nullable = false)
+    private String nome;
+
+    private String endereco;
+}
