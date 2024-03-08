512b87a1a682746e76b8cb7cf42d1caf3a81eeff
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-07 21:06:05 -0300

    feat: add jparepository association

--- src/main/java/br/edu/ipog/consulta/repository/AlunoRepository.java
+++ src/main/java/br/edu/ipog/consulta/repository/AlunoRepository.java
@@ -1,7 +1,9 @@
 package br.edu.ipog.consulta.repository;
 
+import br.edu.ipog.consulta.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository {
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
 }
