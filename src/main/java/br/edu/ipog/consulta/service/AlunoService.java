e59d4ada599b7baa9b9ae8b2ed82543ce199c514
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-07 21:11:22 -0300

    feat: add @Transactional in service layer

--- src/main/java/br/edu/ipog/consulta/service/AlunoService.java
+++ src/main/java/br/edu/ipog/consulta/service/AlunoService.java
@@ -2,6 +2,7 @@ package br.edu.ipog.consulta.service;
 
 import br.edu.ipog.consulta.interfaces.IService;
 import br.edu.ipog.consulta.model.Aluno;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public class AlunoService implements IService<Aluno, Integer>{
 
     @Override
+    @Transactional
     public Aluno create(Aluno entity) {
         return null;
     }
@@ -25,16 +27,19 @@ public class AlunoService implements IService<Aluno, Integer>{
     }
 
     @Override
+    @Transactional
     public Aluno update(Integer id, Aluno entity) {
         return null;
     }
 
     @Override
+    @Transactional
     public Aluno deleteById(Integer id) {
         return null;
     }
 
     @Override
+    @Transactional
     public Aluno delete(Aluno entity) {
         return null;
     }
