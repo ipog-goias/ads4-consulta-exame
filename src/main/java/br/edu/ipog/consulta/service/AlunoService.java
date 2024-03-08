63487da6ba4a55ffb1af0ea1e394fae28e414007
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-07 21:46:33 -0300

    feat: implemented methods of service layer.

--- src/main/java/br/edu/ipog/consulta/service/AlunoService.java
+++ src/main/java/br/edu/ipog/consulta/service/AlunoService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j //anotação responsável para habilitar a parte de logging do projeto.
 @Service //layer service ou camada de negócio.
@@ -22,14 +23,22 @@ public class AlunoService implements IService<Aluno, Integer>{
     public Aluno create(Aluno entity) {
         log.info("Acessando método create na classe {}", AlunoService.class.getSimpleName());
         log.debug("Persistindo dados do aluno: {}", entity);
-        return null;
+
+        return alunoRepository.save(entity); //persiste informação no banco de dados;
     }
 
     @Override
     public Aluno readById(Integer id) {
         log.info("Acessando método readById na classe {}", AlunoService.class.getSimpleName());
         log.debug("Valor recebido foi {}", id);
-        return null;
+
+        Optional<Aluno> alunoEncontrado = alunoRepository.findById(id);
+        if(alunoEncontrado.isPresent()){
+            return alunoEncontrado.get(); //se achou o aluno, retorna o objeto preenchido.
+        }else{
+            log.warn("Identificador id: {} não foi encontrado.", id);
+            return new Aluno(); //se não achou o registro, devolve o objeto 'vazio' (new)
+        }
     }
 
     @Override
