1e4a34efa7be0d9eda53f5e1c245f53f55c48135
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-07 22:16:52 -0300

    feat: implemented methods of resource layer

--- src/main/java/br/edu/ipog/consulta/model/Aluno.java
+++ src/main/java/br/edu/ipog/consulta/model/Aluno.java
@@ -2,7 +2,9 @@ package br.edu.ipog.consulta.model;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data //recurso do lombok para fazer os métodos GET/SET
 @Entity  //ORM gerencia o estado objeto a ser criado à partir da classe concreta
 @Table(name = "ALUNO") //cria uma tabela chamada aluno
 public class Aluno {
